fe3582867d9efdf987c6e425b0d849255efe64bc
Jun Shimura <junshimura@gmail.com>
2025-06-24 10:35:07 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vector = new Vector2(3.0f, 5.0f);
+            var l1 = Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            var l2 = vector.Length();
+            Console.WriteLine($"長さは同じか？{l1}{l1==l2}{l2}");
+
+            var normalizedVector1 = vector/(float)l1;
+            Console.WriteLine($"正規化されたベクトル1: {normalizedVector1}");
+            var normalizedVector2 = Vector2.Normalize(vector);
+            Console.WriteLine($"正規化されたベクトル2: {normalizedVector2}");
+     
+        }
+    }
+}
